17e8503dc7ddad732e5c0a23b632747bdbde706d
Will <Will@HartnellToGo>
2018-08-31 10:43:08 -0400

    added Encode and Decode through comment, not yet implemented

--- BSB/src/CaesarCypher.java
+++ BSB/src/CaesarCypher.java
@@ -5,7 +5,7 @@ public class CaesarCypher {
 	
 	private final static String alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
-	private static String shiftedAlphabet;
+	public static String shiftedAlphabet;
 	
 	public CaesarCypher(int key) {
 		this.key = key;
@@ -13,6 +13,20 @@ public class CaesarCypher {
 		String rightSubstring = alphabet.substring(this.key);
 		shiftedAlphabet = rightSubstring + leftSubstring;
 	}
+	/*public String textEncode(String clearText) {
+		String textEncoded = "";
+		for (int i = 0; i < clearText.length(); i++) {
+		textEncoded += shiftedAlphabet.charAt(alphabet.indexOf(clearText.charAt(i+1)));
+		}
+		return textEncoded;
+	}
 	
-	
+	public String textDecode(String textEncoded) {
+	String textDecoded = "";
+	for(int i = 0; i < textEncoded.length();i++) {
+	textDecoded += alphabet.charAt(shiftedAlphabet.indexOf(textEncoded.charAt(i)));
+	}
+	return textDecoded;
+	}
+	*/
 }
--- BSB/src/Main.java
+++ BSB/src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
-		double sqrt2 = Math.sqrt(2.0);
-		//
+		CaesarCypher shift4 = new CaesarCypher(4);
+		//double sqrt2 = Math.sqrt(2.0);
+		String hi = "potatoisgoodforbody";
+//		String hello = shift4.textEncode(hi);
+		System.out.println("hi");
+		//System.out.println("Here lies:"+ hello);
 	}
 }
