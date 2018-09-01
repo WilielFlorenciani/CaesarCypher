a1d7483111d21bbe4c42b3a8e3c3f39e8935d3d8
WilielFlorenciani <wiliel.florenciani@upr.edu>
2018-08-31 23:29:09 -0400

    Fixed bug with lowercase letters in strings

--- BSB/src/CaesarCypher.java
+++ BSB/src/CaesarCypher.java
@@ -14,6 +14,7 @@ public class CaesarCypher {
 		shiftedAlphabet = rightSubstring + leftSubstring;
 	}
 	public String textEncode(String clearText) {
+		clearText = clearText.toUpperCase();
 		String textEncoded = "";
 		for (int i = 0; i < clearText.length(); i++) {
 		textEncoded += shiftedAlphabet.charAt(alphabet.indexOf(clearText.charAt(i)));
@@ -22,6 +23,7 @@ public class CaesarCypher {
 	}
 	
 	public String textDecode(String textEncoded) {
+	textEncoded = textEncoded.toUpperCase();
 	String textDecoded = "";
 	for(int i = 0; i < textEncoded.length();i++) {
 	textDecoded += alphabet.charAt(shiftedAlphabet.indexOf(textEncoded.charAt(i)));
