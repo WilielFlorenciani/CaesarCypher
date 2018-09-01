2fef216775101cc0a033f5972a54e1a3f4e2ede0
WilielFlorenciani <wiliel.florenciani@upr.edu>
2018-09-01 00:11:50 -0400

    fixed bug that caused runtime error with spaces and numbers

--- BSB/src/CaesarCypher.java
+++ BSB/src/CaesarCypher.java
@@ -17,18 +17,35 @@ public class CaesarCypher {
 		clearText = clearText.toUpperCase();
 		String textEncoded = "";
 		for (int i = 0; i < clearText.length(); i++) {
+		int j = alphabet.indexOf(clearText.charAt(i));
+		if(clearText.charAt(i) == ' ') {
+		textEncoded += clearText.charAt(i);
+		}
+		else if(j != -1) {
 		textEncoded += shiftedAlphabet.charAt(alphabet.indexOf(clearText.charAt(i)));
+		}
+		else
+		{
+		textEncoded += clearText.charAt(i);
+		}
 		}
 		return textEncoded;
 	}
-	
 	public String textDecode(String textEncoded) {
 	textEncoded = textEncoded.toUpperCase();
 	String textDecoded = "";
 	for(int i = 0; i < textEncoded.length();i++) {
+		int f = shiftedAlphabet.indexOf(textEncoded.charAt(i));		//variable f was added as an intermediary due to the method indexOf() not being able to be used in a boolean 
+		if(textEncoded.charAt(i) == ' ') {
+			textDecoded += textEncoded.charAt(i);
+			}
+			else if(f != -1) {
 	textDecoded += alphabet.charAt(shiftedAlphabet.indexOf(textEncoded.charAt(i)));
+	}
+	else {
+		textDecoded += textEncoded.charAt(i);
+	}
 	}
 	return textDecoded;
 	}
-	
 }
--- BSB/src/Main.java
+++ BSB/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		CaesarCypher shift4 = new CaesarCypher(4);
 		//double sqrt2 = Math.sqrt(2.0);
-		String hi = "POTATOISGOODFORBODY";
+		String hi = "123 Adios";
 		String hello = shift4.textEncode(hi);
 		System.out.println(hello);
 		//System.out.println("Here lies:"+ hello);
